845ab41b7346d338b84fa45a2b3b06b510386de3
Elena Payán <elena.payan@kairosds.com>
2021-03-14 00:59:31 +0100

    Add content, styles and funcionality

--- js/main.js
+++ js/main.js
@@ -1,1 +1,3 @@
-console.log("Hola mundo");
+document.getElementById("name").value = "My name";
+document.getElementById("lastName").value = "My last name";
+document.getElementById("avatar").src = "../assets/freestocks-u49hkWzTL3w-unsplash.jpg";
